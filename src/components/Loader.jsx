d632d34b1522d2d9f6948cb6e7702f6a672cd20f
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2025-05-13 17:00:15 +0530

    add loader and api.js

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Loader = () => (
+  <div className="flex justify-center items-center h-64">
+    <p className="text-xl">Loading...</p>
+  </div>
+);
+
+export default Loader;
